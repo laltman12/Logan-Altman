fea1505588dc47054a10d9247648cb5766fc7447
laltman12 <35745737+laltman12@users.noreply.github.com>
2018-01-23 15:37:32 -0600

    Add files via upload

---
+++ loganaltman.c
@@ -0,0 +1,20 @@
+/**
+*Author: Logan Altman
+*Date: 1-23-18
+*
+*this is a test "hello world" program for a repository 
+*test for HW#1
+*/
+
+#include<stdlib.h>
+#include<stdio.h>
+
+int main(int argv, char **argc){
+
+	printf("Logan Altman\n");
+	
+	return 0;
+	
+}
+
+
